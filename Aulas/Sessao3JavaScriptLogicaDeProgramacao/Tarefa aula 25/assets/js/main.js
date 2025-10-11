17aa0121cde30ba9a65814f59b481ba89c3861f0
Diabetico2 <allanjosepereira18@gmail.com>
2025-10-11 11:49:19 -0300

    Codigo upado em 11/10/2025 as 11:49

--- Aulas/Sessao3JavaScriptLogicaDeProgramacao/Tarefa aula 25/assets/js/main.js
+++ Aulas/Sessao3JavaScriptLogicaDeProgramacao/Tarefa aula 25/assets/js/main.js
@@ -1,8 +1,8 @@
 function formulario() {
   const form = document.querySelector("#formulario");
-  const resultado = document.querySelector("#resultado"); 
+  const resultado = document.querySelector("#resultado");
   const info = [];
-  let IMC = 0
+  let IMC = 0;
   function recebeEvento(evento) {
     evento.preventDefault();
     const peso = form.querySelector("#peso");
@@ -13,38 +13,30 @@ function formulario() {
     info.push({
       altura: Number(altura.value),
     });
-    if (peso.value < 10) {
-      resultado.innerHTML = `<p>peso invalido, insira um valor correto</p>`;
-    } else if (altura.value <= 0.9) {
-      resultado.innerHTML = `<p>altura invalida, por favor insira um valor correto</p>`;
+    if (peso.value < 10 || peso.value === null) {
+      resultado.innerHTML = `peso invalida, corrija o valor`;
+    } else if (altura.value < 0.9 || peso.value === null) {
+      resultado.innerHTML = `altura valor invalido, corrija o valor`;
     } else {
-      
-    }
-    IMC = (peso.value / (altura.value * altura.value))
-    if (IMC < 18.5){
-      resultado.innerHTML = `<p>${IMC.toFixed(2)} Tá magro chefe</p>`
-    }
-    
-    else if (IMC > 18.5 && IMC < 24.9){
-      resultado.innerHTML = `<p>${IMC.toFixed(2)} Tá normal</p>`
-    }
-
-    
-    else if (IMC > 25.0 && IMC < 29.9){
-      resultado.innerHTML = `<p>${IMC.toFixed(2)} Ta gordinho ein</p>`
-    }
-
-    
-    else if (IMC > 30.0 && IMC < 39.0){
-      resultado.innerHTML = `<p>${IMC.toFixed(2)} Tá magro chefe</p>`
-    }
-
-    
-    else if (IMC > 40){
-      resultado.innerHTML = `<p>${IMC.toFixed(2)} https://www.youtube.com/watch?v=Zjpgx-nwSGk </p>`
+      IMC = peso.value / (altura.value * altura.value);
+      if (IMC < 18.5) {
+        resultado.innerHTML = `<p>${IMC.toFixed(2)} Abaixo do peso</p>`;
+        console.log("Abaixo do peso");
+      } else if (IMC > 18.5 && IMC < 24.9) {
+        resultado.innerHTML = `<p>${IMC.toFixed(2)} Normal</p>`;
+        console.log("Normal");
+      } else if (IMC > 25.0 && IMC < 29.9) {
+        resultado.innerHTML = `<p>${IMC.toFixed(2)} sobrepeso</p>`;
+        console.log("sobrepeso");
+      } else if (IMC >= 30.0 && IMC < 39.9) {
+        resultado.innerHTML = `<p>${IMC.toFixed(2)} obesidade</p>`;
+        console.log("obesidade");
+      } else if (IMC >= 40.0) {
+        resultado.innerHTML = `<p>${IMC.toFixed(2)} obesidade grave</p>`;
+        console.log("obesidade grave");
+      }
     }
   }
-  console.log(info);
   form.addEventListener("submit", recebeEvento);
 }
 
