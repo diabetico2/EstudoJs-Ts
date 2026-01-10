b12d81c06e1006a5696c9052720510817f2ab928
Diabetico2 <allanjosepereira18@gmail.com>
2026-01-10 10:17:03 -0300

    Codigo upado em 10/01/2026 as 10:17

---
+++ Aulas Javascript Otavio/Sessao3JavaScriptLogicaDeProgramacao/aula31-Desestruturação de Arrays/index.js
@@ -0,0 +1,15 @@
+// const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+
+// const [primeiro, , , terceiro , , quinto , ...resto] = numeros;
+
+// console.log(primeiro);
+// console.log(terceiro);
+// console.log(quinto);
+// console.log(resto);
+const numero = [
+    [1, 2, 3], // 0
+    [4, 5, 6], // 1
+    [7, 8, 9]  // 2  
+]
+console.log(numero[2][1]); // linha 2, coluna 1
