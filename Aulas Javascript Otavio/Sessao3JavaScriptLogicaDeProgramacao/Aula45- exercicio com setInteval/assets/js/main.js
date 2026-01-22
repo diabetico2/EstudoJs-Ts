34692dfa8a326cd68cad5d9d85e2efe44a70976a
Diabetico2 <allanjosepereira18@gmail.com>
2026-01-22 18:21:48 -0300

    Codigo atualizado em 22/01/2026 as 18:21

--- Aulas Javascript Otavio/Sessao3JavaScriptLogicaDeProgramacao/Aula45- exercicio com setInteval/assets/js/main.js
+++ Aulas Javascript Otavio/Sessao3JavaScriptLogicaDeProgramacao/Aula45- exercicio com setInteval/assets/js/main.js
@@ -2,29 +2,44 @@
 
 // 1. Função que só serve para deixar o número bonito (00:00:00)
 function formataTempo(segundos) {
-    // Multiplicamos por 1000 porque o Date usa milissegundos
-    const data = new Date(segundos * 1000); 
-    
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
-    });
+  // Multiplicamos por 1000 porque o Date usa milissegundos
+  const data = new Date(segundos * 1000);
+
+  return data.toLocaleTimeString("pt-BR", {
+    hour12: false,
+    timeZone: "UTC",
+  });
 }
 
 // 2. Selecionando os elementos do HTML
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
+const relogio = document.querySelector(".relogio");
+const iniciar = document.querySelector(".iniciar");
+const pausar = document.querySelector(".pausar");
+const zerar = document.querySelector(".zerar");
 
 // 3. Estado da aplicação (Variáveis globais)
 let segundos = 0; // Guarda a contagem atual
-let timer
+let timer;
 
-iniciar.addEventListener('click', function(){
-   clearInterval(timer)
-   timer = setInterval(function(){
-     segundos ++
-     relogio.innerHTML = formataTempo(segundos)
-   }, 1000)
-})
+document.addEventListener("click", function (e) {
+  zs
+  if (el.classList.contains("zerar")) {
+    relogio.style.color = "black";
+    setTimeout(function () {
+      clearInterval(timer);
+    });
+    segundos = 0
+  }
+  if (el.classList.contains("pausar")) {
+    clearInterval(timer);
+    relogio.style.color = "red";
+  }
+  if (el.classList.contains("iniciar")) {
+    clearInterval(timer);
+    timer = setInterval(function () {
+      relogio.style.color = "black";
+      segundos++;
+      relogio.innerHTML = formataTempo(segundos);
+    }, 1000);
+  }
+});
