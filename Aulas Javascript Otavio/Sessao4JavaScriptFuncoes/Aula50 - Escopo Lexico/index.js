2c59b902c00e2b6d0c893acf92a4fe43713ced00
diabetico2 <allanjosepereira18@gmail.com>
2026-01-29 11:00:33 -0300

    Codigo atualizado em 29/01/2026 as 11:00

---
+++ Aulas Javascript Otavio/Sessao4JavaScriptFuncoes/Aula50 - Escopo Lexico/index.js
@@ -0,0 +1,11 @@
+const nome = 'Luiz'
+
+function falaNome(){
+    console.log(nome)
+}
+
+function usaFalaNome(){
+        const nome = 'otavio'
+    falaNome()
+}
+usaFalaNome()
---
+++ Aulas Javascript Otavio/Sessao4JavaScriptFuncoes/Aula52 - funcao de callback/index.js
@@ -0,0 +1,19 @@
+function f1(){
+    console.log('f1')
+}
+
+function f2(){
+    console.log('f1')
+}
+
+function f3(){
+    console.log('f1')
+}
+
+function f4(){
+    console.log('f1')
+}
+f1()
+f2()
+f3()
+console.log('olá mundo!')
