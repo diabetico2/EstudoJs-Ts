3a833f07382d19ca09867610d212455100c96491
Diabetico2 <allanjosepereira18@gmail.com>
2026-01-31 11:41:56 -0300

    Codigo atualizado em 31/01/2026 as 11:41

--- Aulas Javascript Otavio/Sessao4JavaScriptFuncoes/Aula54 - funcao Fabrica (não concluido/index.js
+++ Aulas Javascript Otavio/Sessao4JavaScriptFuncoes/Aula54 - funcao Fabrica (não concluido/index.js
@@ -4,16 +4,23 @@ function criaPessoa(nome, sobrenome, a, p) {
   return {
     nome,
     sobrenome,
-    nomeCompleto(){
-      return `${nome} ${sobrenome}`
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
     },
-    fala: function (assunto) {
-      return `${this.nome} está ${assunto}`;
+    set nomeCompleto(valor) {
+      valor = valor.split(' ')
+      this.nome = valor.shift()
+      this.sobrenome = valor.join(' ')
+      console.log(valor)
+    },
+    fala: function (assunto = 'Falando sobre nada') {
+      return `${this.nome} está ${assunto}`;  
     },
     altura: a,
     peso: p,
     //getter
-    get imc() { // passa a fingir ser um atributo
+    get imc() {
+      // passa a fingir ser um atributo
       const indice = this.peso / this.altura ** 2;
       return indice.toFixed();
     },
@@ -21,4 +28,8 @@ function criaPessoa(nome, sobrenome, a, p) {
 }
 
 const p1 = criaPessoa("allan", "jose", 1.8, 90);
-console.log(p1.nomeCompleto())
+p1.nomeCompleto = "José dos Santos";
+console.log(p1.nomeCompleto);
+console.log(p1.nome);
+console.log(p1.sobrenome);
+console.log(p1.fala());
