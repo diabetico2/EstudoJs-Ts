146dff4cc956859254c01c5471896d387bfb0922
diabetico2 <allanjosepereira18@gmail.com>
2026-02-06 14:54:21 -0300

    Codigo atualizado em 06/02/2026 as 14:54

--- Aulas Javascript Otavio/Sessao4JavaScriptFuncoes/Aula57 - Exercicio construction/assets/js/main.js
+++ Aulas Javascript Otavio/Sessao4JavaScriptFuncoes/Aula57 - Exercicio construction/assets/js/main.js
@@ -1,28 +1,25 @@
 function Calculadora() {
-  
-    this.display = document.querySelector(".display"),
-    this.btnClear = document.querySelector(".btn-clear"),
-
-this.inicia = () => {
-    this.cliqueBotoes();
-    this.pressionaEnter();
+  ((this.display = document.querySelector(".display")),
+    (this.btnClear = document.querySelector(".btn-clear")),
+    (this.inicia = () => {
+      this.cliqueBotoes();
+      this.pressionaEnter();
+    }));
+  this.pressionaEnter = () => {
+    document.addEventListener("keyup", (e) => {
+      if (e.key === "Enter") {
+        this.realizaConta();
+      }
+    });
   };
-this.pressionaEnter = () => {
-      // Mude de this.display para document
-      document.addEventListener("keyup", (e) => { 
-        if (e.key === 'Enter') {
-          this.realizaConta();
-        }
-      });
-    };
 
-    this.clearDisplay = () => {
-      this.display.value = "";
-    },
-    this.apagaUm = () => {
+  ((this.clearDisplay = () => {
+    this.display.value = "";
+  }),
+    (this.apagaUm = () => {
       this.display.value = this.display.value.slice(0, -1);
-    },
-    this.realizaConta = () => {
+    }),
+    (this.realizaConta = () => {
       let conta = this.display.value;
 
       try {
@@ -36,24 +33,21 @@ this.pressionaEnter = () => {
         alert("conta invalida");
         return;
       }
-    },
+    }),
+    (this.cliqueBotoes = () => {
+      document.addEventListener("click", (event) => {
+        const el = event.target;
+        if (el.classList.contains("btn-num")) this.btnParaDisplay(el.innerText);
+        if (el.classList.contains("btn-clear")) this.clearDisplay();
+        if (el.classList.contains("btn-del")) this.apagaUm();
+        if (el.classList.contains("btn-eq")) this.realizaConta();
 
-   this.cliqueBotoes = () => {
-    document.addEventListener("click", (event) => {
-      const el = event.target;
-      if (el.classList.contains("btn-num")) this.btnParaDisplay(el.innerText);
-      if (el.classList.contains("btn-clear")) this.clearDisplay();
-      if (el.classList.contains("btn-del")) this.apagaUm();
-      if (el.classList.contains("btn-eq")) this.realizaConta();
-      
-      // Dica: focar no display após clicar para o Enter funcionar melhor
-      this.display.focus();
-    });
-  };
+        this.display.focus();
+      });
+    }));
 
-  this.btnParaDisplay = (valor) => this.display.value += valor;
+  this.btnParaDisplay = (valor) => (this.display.value += valor);
 }
 
-// Para usar, você PRECISA da palavra 'new'
 const calculadora = new Calculadora();
 calculadora.inicia();
--- Aulas Javascript Otavio/Sessao4JavaScriptFuncoes/Aula57 - Exercicio construction/assets/js/mainProfessor.js
+++ Aulas Javascript Otavio/Sessao4JavaScriptFuncoes/Aula57 - Exercicio construction/assets/js/mainProfessor.js
@@ -1,5 +1,5 @@
 function Calculadora() {
-  this.display = document.querySelector('.display');
+  this.display = document.querySelector(".display");
 
   this.inicia = () => {
     this.capturaCliques();
@@ -7,7 +7,7 @@ function Calculadora() {
   };
 
   this.capturaEnter = () => {
-    document.addEventListener('keyup', e => {
+    document.addEventListener("keyup", (e) => {
       if (e.keyCode === 13) {
         this.realizaConta();
       }
@@ -15,12 +15,12 @@ function Calculadora() {
   };
 
   this.capturaCliques = () => {
-    document.addEventListener('click', event => {
+    document.addEventListener("click", (event) => {
       const el = event.target;
-      if (el.classList.contains('btn-num')) this.addNumDisplay(el);
-      if (el.classList.contains('btn-clear')) this.clear();
-      if (el.classList.contains('btn-del')) this.del();
-      if (el.classList.contains('btn-eq')) this.realizaConta();
+      if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-clear")) this.clear();
+      if (el.classList.contains("btn-del")) this.del();
+      if (el.classList.contains("btn-eq")) this.realizaConta();
     });
   };
 
@@ -28,25 +28,25 @@ function Calculadora() {
     try {
       const conta = eval(this.display.value);
 
-      if(!conta) {
-        alert('Conta inválida');
+      if (!conta) {
+        alert("Conta inválida");
         return;
       }
 
       this.display.value = conta;
-    } catch(e) {
-      alert('Conta inválida');
+    } catch (e) {
+      alert("Conta inválida");
       return;
     }
   };
 
-  this.addNumDisplay = el => {
+  this.addNumDisplay = (el) => {
     this.display.value += el.innerText;
     this.display.focus();
   };
 
-  this.clear = () => this.display.value = '';
-  this.del = () => this.display.value = this.display.value.slice(0, -1);
+  this.clear = () => (this.display.value = "");
+  this.del = () => (this.display.value = this.display.value.slice(0, -1));
 }
 
 const calculadora = new Calculadora();
