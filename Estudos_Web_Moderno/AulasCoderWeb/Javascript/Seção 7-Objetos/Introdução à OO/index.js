0117a4f19647fc06e87af47db27ee8b8065741e3
diabetico2 <allanjosepereira18@gmail.com>
2026-02-26 16:25:00 -0300

    Codigo atualizado em 26/02/2026 as 16:25

--- Estudos_Web_Moderno/AulasCoderWeb/Javascript/Seção 7-Objetos/Introdução à OO/index.js
+++ Estudos_Web_Moderno/AulasCoderWeb/Javascript/Seção 7-Objetos/Introdução à OO/index.js
@@ -1,3 +1,19 @@
 // CODIGO NÃO EXECUTAVEL
 
 // Procedural
+processamento(v1, v2, v3);
+
+//OO
+objeto = {
+  v1,
+  v2,
+  v3,
+  processamento() {},
+};
+objeto.processamento() // foco passou a ser o objeto
+
+// principios importantes
+// abstracao
+// encaspsulamento
+// herança
+// poliformismo
