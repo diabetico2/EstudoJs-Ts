6b380970b24e622057fe595261b988aab1565c42
Diabetico2 <allanjosepereira18@gmail.com>
2026-03-23 20:04:35 -0300

    Codigo atualizado em 23/03/2026 as 20:04

--- Estudos_Web_Moderno/AulasCoderWeb/Javascript/PDF exercicios/Exercicios_JS/17_somar_numeros.js
+++ Estudos_Web_Moderno/AulasCoderWeb/Javascript/PDF exercicios/Exercicios_JS/17_somar_numeros.js
@@ -1,1 +1,8 @@
 // Exercicio extraido do PDF de Fundamentos de Programacao 
+const soma = function(...v1){
+    const juntar = (ac,at) => ac + at
+    const resul = v1.reduce(juntar)
+    return resul
+}
+
+console.log(soma(5,6))
