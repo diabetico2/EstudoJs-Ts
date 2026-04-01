caa570a672d3616dcfb65714c6160eb5e438d77f
diabetico2 <allanjosepereira18@gmail.com>
2026-04-01 16:39:39 -0300

    Codigo atualizado em 01/04/2026 as 16:39

--- Estudos_Web_Moderno/AulasCoderWeb/Javascript/Seção 9 - node/funcionarios/index.js
+++ Estudos_Web_Moderno/AulasCoderWeb/Javascript/Seção 9 - node/funcionarios/index.js
@@ -1,17 +1,31 @@
 // ttp://files.cod3r.com.br/curso-js/funcionarios.json
 
-const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
-const axios = require('axios')
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
+const axios = require("axios");
 
+const cidadeM = (m) => m.pais === "Russia";
+const generoM = (m) => m.genero === "M";
+const salarioM = (v, vAtual) => {
+  return v.salario > vAtual.salario ? v : vAtual;
+};
+//
+const cidadania = (f) => f.pais === "China";
+const genero = (f) => f.genero === "F";
+const salario = (v, vAtual) => {
+  return v.salario < vAtual.salario ? v : vAtual;
+};
 
-const chineses = f => f.pais === 'China'
-const mulheres = f => f.genero === 'F'
-const salario =  ((fun, funAtual) =>{
-    return fun.salario < funAtual.salario? fun: funAtual
-})
-
-axios.get(url).then(response =>{ // ele pega os dados da URL json ( get ) e dai realiza uma ação (then)
-    const funcionarios = response.data // o funcionarios recebe toda as infos de response
-    console.log(funcionarios)
-})
+axios.get(url).then((response) => {
+  // ele pega os dados da URL json ( get ) e dai realiza uma ação (then)
+  const funcionarios = response.data; // o funcionarios recebe toda as infos de response
+  const resul = funcionarios.filter(cidadania).filter(genero).reduce(salario);
+  const resulM = funcionarios.filter(cidadeM).filter(generoM).reduce(salarioM)
+  console.log(resul);
+});
 // pegar a mulher chinesa com o menor salario
+
+axios.get(url).then((response) => {
+  const funcionarios = response.data; 
+  const resulM = funcionarios.filter(cidadeM).filter(generoM).reduce(salarioM);
+  console.log(resulM)
+});
